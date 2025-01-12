dfd78abb038fa798ddf7002b38fdd0ac55fc76e7
Siddharth Mishra <siddharthbigshows@gmail.com>
2025-01-12 19:16:57 +0530

    admin login page

--- site/src/pages/adminlogin.js
+++ site/src/pages/adminlogin.js
@@ -4,13 +4,20 @@ import './adminlogin.css';
 
 function Adminlogin(){
     return(
-        <div id="loginformbox" >
-            <h1>Admin Login</h1><br></br>
+        <div id="loginpage" >
 
-            <input type="text" name="username" placeholder="Username" /><br></br>
-            <input type="password" name="password" placeholder="Password" /><br></br>
+            <div id="loginformbox" >
+                <h1>Admin Login</h1><br></br>
 
-            <button>Login</button>
+                <input type="text" name="username" placeholder="Username" /><br></br>
+                <input type="password" name="password" placeholder="Password" /><br></br>
+
+               <button>Login</button>
+            </div>
+
+            <img src="/images/person_phone.png" alt="o" />
+
+
 
 
 
